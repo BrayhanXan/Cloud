e7f1e44d69c3341db85676870e14fc9d2b832f8c
JoelVargasP <130510943+JoelVargasP@users.noreply.github.com>
2023-04-20 16:34:43 -0500

    Update SimuladorElecciones.java
    Ordenación de resultados

--- SimuladorElecciones.java
+++ SimuladorElecciones.java
@@ -19,24 +19,24 @@ Imprimir "5. Joel"
 Leer cantidad_votos
 
 //Comenzar a hacer la simulación del voto
-Para (i = 1, i ≤ cantidad_votos, i++) {
+Para (i = 1, i ≤ cantidad_votos, i++) hacer
     Imprimir "Para el votante " i " su voto es para (ingrese el número correspondiente al candidato):"
     Leer int voto
     
     //Contar votos para los candidatos
-    si (voto == 1) {
+    si (voto == 1) entonces
         c1 = c1 + 1
-    } si no (voto == 2) {
+    } si no (voto == 2) entonces
         c2 = c2 + 1
-    } si no (voto == 3) {
+    } si no (voto == 3) entonces
         c3 = c3 + 1
-    } si no (voto == 4) {
+    } si no (voto == 4) entonces
         c4 = c4 + 1
-    } si no (voto == 5) {
+    } si no (voto == 5) entonces
         c5 = c5 + 1
     }
-    }fin si
-}fin para
+    fin si
+fin para
 
 //Mostrar los resultados de la simulación
 Imprimir "Los resultados de la simulación son:"
@@ -46,8 +46,34 @@ Imprimir "Daniel: " + c3
 Imprimir "Brayhan: " + c4
 Imprimir "Joel: " + c5
 
-//Organizar candidatos de mayor a menor votación
+//Para organizar la presentación de votos
 
+// Definir los arreglos de candidatos y votos
+candidatos = {"Hollman", "Fernando", "Daniel", "Brayhan", "Joel"}
+int votos = {c1, c2, c3, c4, c5}
+
+// Ordenar candidatos según votos
+
+Para (i=0, i<longitud(candidatos) - 1, i++) hacer
+    Para (j=i+1, j<longitud(candidatos), j++ hacer
+    
+    Si (votos[i] < votos[j]) entonces
+        int tempV = votos[i]                // Intercambiar votos
+        votos[i] = votos[j]
+        votos[j] = tempV    
+
+        cadena tempC = candidatos[i]          // Intercambiar candidatos
+        candidatos[i] = candidatos[j]
+        candidatos[j] = tempC
+    fin si
+    fin para
+fin para
+
+//Mostrar resultados finales ordenados
 Imprimir "Resultados Elecciones CUN"
+    Para (i=0, i < longitud(candidatos), i++) hacer
+        Imprimir i+1 + " candidato" : "votos"
+    Fin Para
+
 
 Final
